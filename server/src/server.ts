5263807e6ed3f42dbfff65a40d9a58e90bf02acd
jfernandesdev <jfernandes.dev@gmail.com>
2022-11-04 17:32:31 -0300

    FEAT: routes user count and guess count

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,6 +42,18 @@ async function bootstrap() {
     return reply.status(201).send({ code })
   })
 
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+
   await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
 }
 
